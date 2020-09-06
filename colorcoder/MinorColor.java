59b315c1f10ad4861b502777f8f0b8cff4750dee
sanjeev-nnn <69623115+sanjeev-nnn@users.noreply.github.com>
2020-09-06 23:00:01 +0530

    Update MinorColor.java

--- colorcoder/MinorColor.java
+++ colorcoder/MinorColor.java
@@ -13,12 +13,12 @@ public enum MinorColor {
     int getIndex() {
         return index;
     }
-    public static MinorColor fromIndex(int index) {
+/*    public static MinorColor fromIndex(int index) {
         for(MinorColor color: MinorColor.values()) {
             if(color.getIndex() == index) {
                 return color;
             }
         }
         return null;
-    }
+    } */
 }
