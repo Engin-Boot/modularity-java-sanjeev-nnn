b04d7f344e4bf188e69ef9e07847446381b7135d
sanjeev-nnn <69623115+sanjeev-nnn@users.noreply.github.com>
2020-09-06 23:21:20 +0530

    Update ColorNames.java

--- colorcoder/ColorNames.java
+++ colorcoder/ColorNames.java
@@ -5,6 +5,7 @@ public class ColorNames {
             "White", "Red", "Black", "Yellow", "Violet"
     };
     final static int numberOfMajorColors = MajorColorNames.length;
+  
     final static String MinorColorNames[] = {
             "Blue", "Orange", "Green", "Brown", "Slate"
     };
